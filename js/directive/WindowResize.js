1bba27e8a5f0f877e370e03d19359d66bd17d8b5
Carey Hinoki <carey.hinoki@gmail.com>
2012-10-05 01:58:18 -0700

    Fixed utility render on window scroll + added responsive handling for play elements

---
+++ js/directive/WindowResize.js
@@ -0,0 +1,33 @@
+angular.module('CareyHinoki').
+	directive('windowResize', function () {
+		return {
+			link: function (scope, element, attrs) {
+				function debounce(func, threshold) {
+	                var timeout;
+	                
+	                return function () {
+	                    var obj = this,
+	                    	args = arguments;
+
+	                    $('#utility').hide();
+
+	                    if (timeout) {
+	                        clearTimeout(timeout);
+	                    }
+	                    
+	                    timeout = setTimeout(function() {
+	                        func.apply(obj, args);
+	                        
+	                        timeout = null;
+	                    }, threshold || 250);
+	                };
+	            }
+
+			    function onResize(event) {
+			    	$('#utility').fadeIn(400);
+			    }
+
+				$(window).on('resize', debounce(onResize, 800));
+			}
+		};
+	});
--- js/script.js
+++ js/script.js
@@ -2192,6 +2192,39 @@ angular.module('CareyHinoki').
 		};
 	});
 angular.module('CareyHinoki').
+	directive('windowResize', function () {
+		return {
+			link: function (scope, element, attrs) {
+				function debounce(func, threshold) {
+	                var timeout;
+	                
+	                return function () {
+	                    var obj = this,
+	                    	args = arguments;
+
+	                    $('#utility').hide();
+
+	                    if (timeout) {
+	                        clearTimeout(timeout);
+	                    }
+	                    
+	                    timeout = setTimeout(function() {
+	                        func.apply(obj, args);
+	                        
+	                        timeout = null;
+	                    }, threshold || 250);
+	                };
+	            }
+
+			    function onResize(event) {
+			    	$('#utility').fadeIn(400);
+			    }
+
+				$(window).on('resize', debounce(onResize, 800));
+			}
+		};
+	});
+angular.module('CareyHinoki').
 	directive('windowScroll', function () {
 		return {
 			link: function (scope, element, attrs) {
